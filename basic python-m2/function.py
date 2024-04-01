9654809f9e9af29a23e49b3e0ee7edc519a1646e
zisan <mashroorzisan@gmail.com>
2024-04-01 18:06:30 +0600

    module-2

---
+++ basic python-m2/function.py
@@ -0,0 +1,11 @@
+# python is a dynamically typed programming language
+# function definition
+# define
+
+def double_it(num1):
+    result = num1*2
+    print(result)
+
+
+double_it(34)
+double_it(4)
